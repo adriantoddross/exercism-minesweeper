ae55c973286e3fd345717eca52efe6c7b2143937
Adrian Ross <adriantoddross@gmail.com>
2021-03-11 11:56:27 -0500

    Brainstorming and pseudocode

--- minesweeper.js
+++ minesweeper.js
@@ -4,9 +4,19 @@ export const annotate = (input) => {
   if (input.length <= 1) return input;
   if (!input.includes("*") || !input.includes(" ")) return input;
 
-  // handling mine surrounded by spaces
-  // when looping through a row, stop at empty space
-  // count the number of mines from the beginning of string to empty space
-  // count the number of mines from end of string to empty space
-  // Replace the space with the sum of those numbers
+  /*
+  
+  Ideas:
+  - Create a .map statement
+  - Init variable w/ concatenated strings, at some point.
+  
+   handling mine surrounded by spaces:
+
+   when looping through a row, stop at empty space
+   grab index of current string, count the number of mines from the beginning of string to empty space
+   count the number of mines from end of string to empty space
+   - This causes a problem. How can I keep track of index in the array's string vs index in the concatenated string? May become tricky.
+   Replace the current space with the sum of those numbers
+   Continue loop until end of string
+  */
 };
